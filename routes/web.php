fda6a0b8bc7ee75e5f91ead0e5bbf0be505d8353
melky <melkytam17@gmail.com>
2024-06-16 23:12:17 -0300

    feat/authorizations-crud

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,31 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ScheduledClassController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', DashboardController::class)->middleware(['auth'])->name('dashboard');
+
+Route::get('/instructor/dashboard', function () {
+    return view('instructor.dashboard');
+})->middleware(['auth', 'role:instructor'])->name('instructor.dashboard');
+
+Route::resource('/instructor/schedule', ScheduledClassController::class)
+    ->only('index', 'create', 'store', 'destroy')
+    ->middleware(['auth', 'role:instructor']);
+
+Route::get('/member/dashboard', function () {
+    return view('member.dashboard');
+})->middleware(['auth', 'role:member'])->name('member.dashboard');
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth', 'role:admin'])->name('admin.dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
