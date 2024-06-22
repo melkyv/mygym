509032d7d7f94a8b20b9139b37db13d7e3bfefa7
melky <melkytam17@gmail.com>
2024-06-21 22:51:24 -0300

    Add seeders and factories

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Shruti',
+            'email' => 'shruti@example.com'
+        ]);
+
+        User::factory()->create([
+            'name' => 'Taylor',
+            'email' => 'taylor@example.com'
+        ]);
+
+        User::factory()->create([
+            'name' => 'John',
+            'email' => 'john@example.com',
+            'role' => 'instructor'
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'role' => 'admin'
+        ]);
+
+        User::factory()->count(10)->create();
+
+        User::factory()->count(10)->create([
+            'role' => 'instructor'
+        ]);
+    }
+}
