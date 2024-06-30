ea6554451fe1e04da3fa31d876723a73be797c18
melky <melkytam17@gmail.com>
2024-06-30 14:28:32 -0300

    feat: send notifications

--- app/Listeners/NotifyClassCanceled.php
+++ app/Listeners/NotifyClassCanceled.php
@@ -4,9 +4,11 @@ namespace App\Listeners;
 
 use App\Events\ClassCanceled;
 use App\Mail\ClassCanceledMail;
+use App\Notifications\ClassCanceledNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Notification;
 
 class NotifyClassCanceled
 {
@@ -23,15 +25,17 @@ class NotifyClassCanceled
      */
     public function handle(ClassCanceled $event): void
     {
-        $members = $event->scheduledClass->members();
+        $members = $event->scheduledClass->members()->get();
 
         $className = $event->scheduledClass->classType->name;
         $classDateTime = $event->scheduledClass->date_time;
 
         $details = compact('className', 'classDateTime');
 
-        $members->each(function ($member) use ($details){
-            Mail::to($member)->send(new ClassCanceledMail($details));
-        });
+        // $members->each(function ($member) use ($details){
+        //     Mail::to($member)->send(new ClassCanceledMail($details));
+        // });
+
+        Notification::send($members, new ClassCanceledNotification($details));
     }
 }
