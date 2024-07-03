25bad4605d9af7739c14d423d8572e0564b65133
melky <melkytam17@gmail.com>
2024-07-02 21:34:20 -0300

    feat: test-driven development

--- app/Policies/ScheduledClassPolicy.php
+++ app/Policies/ScheduledClassPolicy.php
@@ -12,6 +12,7 @@ class ScheduledClassPolicy
      */
     public function delete(User $user, ScheduledClass $scheduledClass)
     {
-        return $user->id === $scheduledClass->instructor_id;
+        return $user->id === $scheduledClass->instructor_id
+        && $scheduledClass->date_time > now()->addHours(2);
     }
 }
